a37b780f93e3094d8c1babde65fa0109cba84c21
Jai Prakash Yadav <jaipyadav07@gmail.com>
2023-09-03 13:04:34 +0530

    decode json in golang using marshal

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ type course struct {
 
 func main() {
 	fmt.Println("It is a JSON")
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -34,4 +35,37 @@ func EncodeJson() {
 	}
 
 	fmt.Printf("%s", finalJson)
+}
+
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"name": "TeactJs",
+		"Price": 299,
+		"Platform": "LearnCodeonline.in",
+		"Tags": ["web-dev","js"]
+    }
+	`)
+
+	var lcoCourse course
+
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("Json is Valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		// fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("Not valid json")
+	}
+
+	// some cases key value pair
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	// fmt.Println("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("key is %v and value is %v", k, v)
+	}
 }
